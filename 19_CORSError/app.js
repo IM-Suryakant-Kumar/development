643c416e9656aac4856bc8426e84c1612bb7b986
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-03-08 13:56:22 +0530

    practice: Updated CORSError

--- 19_CORSError/app.js
+++ 19_CORSError/app.js
@@ -10,6 +10,13 @@ app.post("/", (req, res) => {
   res.sendFile(__dirname + "/data.json");
 });
 
+// Verification
+app.options("/", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.send("Okay");
+});
+
 app.listen(3000, () => {
   console.log("Listing to port: 3000.");
 });
--- 19_CORSError/script.js
+++ 19_CORSError/script.js
@@ -3,6 +3,9 @@ let url = "http://localhost:3000";
 // Fetch (Simple Request)
 fetch(url, {
   method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
   body: "Yo!!",
 })
   .then((response) => {
