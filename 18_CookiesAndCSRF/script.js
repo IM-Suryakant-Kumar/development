7bad551aef19dc709a6e167ddcc897cd0aff9514
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-10-18 21:56:18 +0530

    Starting Practice-1

--- 18_CookiesAndCSRF/script.js
+++
@@ -1,5 +0,0 @@
-// document.cookie = "name=pepcoder; ";
-// let date = new Date();
-// document.cookie = `name=peppy; expires = ${date.toUTCString()}`;
-document.cookie = `name=peppy; secure; samesite=strict;`;
-console.log(document.cookie);
