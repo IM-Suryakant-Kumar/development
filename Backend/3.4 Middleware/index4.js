c222844c33a22a7603312c4bba6dde3bba179a95
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-03-07 13:03:55 +0530

    completed 3.4 index4.js

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,8 +1,23 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/submit", (req, res) => {
+  const {street, pet} = req.body;
+  res.send(`<h1>Your brand name is: </h1><h2>${street + pet}</h2>`)
+})
+
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
