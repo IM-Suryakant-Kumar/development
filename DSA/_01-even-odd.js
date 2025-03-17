d13b7d56ff320bc33c8a975736523529b1082f04
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-03-17 15:59:19 +0530

    feat: Added print table

--- DSA/_01-even-odd.js
+++ DSA/_01-even-odd.js
@@ -1,19 +1,19 @@
 // approach - by finding the remainder
 function isEven(n) {
-  if(n % 2 === 0) return true;
-  else return false;
+	if (n % 2 === 0) return true;
+	else return false;
 }
 
 // approach - Using bitwise AND operator
 function isEven(n) {
-  if((n & 1) === 0) return true
-  else return false;
+	if ((n & 1) === 0) return true;
+	else return false;
 }
 
 // approach - Using bitwise shift operator
 function isEven(n) {
-  if(n === (n >> 1) << 1) return true
-  else return false;
+	if (n === (n >> 1) << 1) return true;
+	else return false;
 }
 
 console.log(isEven(4));
