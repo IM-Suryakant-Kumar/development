560eab7463fcd829971eb44311f4c8f360a3aa0f
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-14 14:58:19 +0530

    refactor: removedsome files

--- projects/social-media/client/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Layout } from "./layouts/Layout";
-export {default as HostLayout } from "./layouts/HostLayout";
-export {default as AuthLayout } from "./layouts/AuthLayout";
--- projects/social-media/client/src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Explore } from "./Explore";
-export { default as Profile } from "./Profile";
-export { default as Settings } from "./Settings";
-export { default as Post } from "./Post";
-export { default as Login } from "./Login";
-export { default as Signup } from "./Signup";
