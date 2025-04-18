51ee3af3c83e88feca6fd72ca7c8206f8e0ba161
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-18 17:03:32 +0530

    feat: Added home

--- projects/01-cs/src/app.ts
+++ projects/01-cs/src/app.ts
@@ -1,9 +1,9 @@
-import "express-async-errors";
 import "dotenv/config";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import { errorHandleMiddleware, notFoundMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -13,12 +13,17 @@ const PORT = process.env.PORT || 4000;
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(morgan("tiny"));
 
-async () => {
+// error handler
+app.use(notFoundMiddleware);
+app.use(errorHandleMiddleware);
+(async () => {
+  app.listen(PORT, () => console.log(`App is running at http://localhost:${PORT}`));
 	try {
-		app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
 	} catch (error) {
 		console.log(error);
 	}
-};
+})();
