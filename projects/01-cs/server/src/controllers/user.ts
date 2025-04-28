7348211b547676a567ee968236363fce43dcefed
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-29 01:08:56 +0530

    feat: Updated user  controller

--- projects/01-cs/server/src/controllers/user.ts
+++ projects/01-cs/server/src/controllers/user.ts
@@ -22,3 +22,31 @@ export const updateProfile = asyncWrapper(async (req: IReq, res: Response) => {
   const user = await User.findByIdAndUpdate(req.user?._id, req.body, { new: true });
   res.status(200).json({ success: true, user});
 });
+
+export const createFollowing = asyncWrapper(async (req: IReq, res: Response) => {
+	// following
+	const user = await User.findByIdAndUpdate(
+		req.user?._id,
+		{ $push: { followings: req.params.userId } },
+		{ new: true }
+	).populate("followings");
+
+	// followers
+	await User.findByIdAndUpdate(req.params.userId, { $push: { followers: req.user?._id } });
+
+	res.status(201).json({ success: true, user });
+});
+
+export const deleteFollowing = asyncWrapper(async (req: IReq, res: Response) => {
+	//following
+	const user = await User.findByIdAndUpdate(
+		req.user?._id,
+		{ $pull: { followings: req.params.userId } },
+		{ new: true }
+	);
+
+	// followers
+	await User.findByIdAndUpdate(req.params.userId, { $pull: { followers: req.user?._id } });
+
+	res.status(202).json({ success: true, user });
+});
