7f453d7173040fe45a40dd7b73786c6821bee4c8
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-05-12 00:30:48 +0530

    feat: Added NotFound page

--- projects/ccpp4/client/src/pages/Landing.tsx
+++ projects/ccpp4/client/src/pages/Landing.tsx
@@ -4,22 +4,26 @@ import { Link } from "react-router";
 export const Landing = () => {
 	return (
 		<article className="bg-secondary">
-			<section className="w-[95%] max-w-[90%] mx-auto min-h-screen flex flex-col justify-center items-center gap-4 md:flex-row md:justify-around">
-				<img
-					className="w-[90%] md:w-[40%] object-contain md:order-2"
-					src="/notebook.svg"
-					alt="notebook"
-				/>
+			<section className="w-[95%] max-w-[90%] mx-auto min-h-screen flex flex-col justify-center items-center gap-6 md:flex-row md:justify-around text-center">
+				<section className="flex justify-center items-center gap-4 md:order-2">
+					<img
+						className="object-contain md:w-xl"
+						src="/notebook.svg"
+						alt="notebook"
+						width={400}
+						height={400}
+					/>
+				</section>
 				<section className="flex flex-col justify-center items-center gap-4 md:order-1">
-					<h1 className="text-logo font-secondary text-xl md:text-2xl font-semibold border-l-4 border-r-4 rounded-md px-8">
+					<h1 className="text-logo font-secondary text-2xl font-semibold border-l-4 border-r-4 rounded-md px-8">
 						ClechKeep
 					</h1>
-					<GiPencil className="w-10 h-10 fill-logo" />
-					<p className="max-w-xs text-gray-600 text-sm sm:text-xl text-center">
+					<GiPencil className="fill-logo" size="2em" />
+					<p className="max-w-xs text-gray-600 text-lg">
 						A modern way to Keep and handle your notes digitally
 					</p>
 					<Link
-						className="w-40 h-7 md:w-50 md:h-8 bg-logo text-primary flex justify-center items-center text-xs md:text-sm font-secondary font-bold rounded-xs"
+						className="w-40 h-8 bg-logo text-primary flex justify-center items-center text-sm font-secondary font-bold rounded-xs"
 						to="/note"
 					>
 						Get Started
--- projects/ccpp4/client/src/pages/NotFound.tsx
+++ projects/ccpp4/client/src/pages/NotFound.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router";
+
 export const NotFound = () => {
-  return (
-    <>NotFound</>
-  )
-}
+	return (
+		<article className="w-[95%] md:w-[95%] min-h-screen flex flex-col justify-center items-center gap-4">
+			<h1 className="text-gray-400 text-xl font-bold text-center md:text-2xl">
+				404 - PAGE NOT FOUND!
+			</h1>
+			<Link
+				className="w-40 h-8 bg-logo text-primary flex justify-center items-center font-secondary font-semibold rounded-xs"
+				to="/note"
+			>
+				Go to Note
+			</Link>
+		</article>
+	);
+};
