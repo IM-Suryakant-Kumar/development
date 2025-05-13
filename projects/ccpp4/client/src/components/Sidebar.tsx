1cad6f06cf742e6ed3d2991a7e414f94f72e7eae
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-05-13 09:35:39 +0530

    feat: Added Sidebar

--- projects/ccpp4/client/src/components/Sidebar.tsx
+++ projects/ccpp4/client/src/components/Sidebar.tsx
@@ -1,25 +1,34 @@
-import { BsTrash } from "react-icons/bs"
-import { IoArchiveOutline } from "react-icons/io5"
-import { TfiPencilAlt2 } from "react-icons/tfi"
-import { NavLink } from "react-router"
+import { BsTrash } from "react-icons/bs";
+import { MdOutlineArchive } from "react-icons/md";
+import { TfiPencilAlt2 } from "react-icons/tfi";
+import { NavLink } from "react-router";
 
 export const Sidebar = () => {
-  return (
-    <article className="bg-secondary w-full h-15 fixed bottom-0 flex items-center">
-      <section className="wrapper flex items-center justify-between">
-        <NavLink to="/note" className="">
-          <TfiPencilAlt2 className="fill-logo" />
-          <span className="hidden md:block">Note</span>
-        </NavLink>
-        <NavLink to="/archive" className="">
-          <IoArchiveOutline className="fill-logo" />
-          <span className="hidden md:block">Archive</span>
-        </NavLink>
-        <NavLink to="/trash" className="">
-          <BsTrash className="fill-logo" />
-          <span className="hidden md:block">Trash</span>
-        </NavLink>
-      </section>
-    </article>
-  )
-}
+	return (
+		<article className="bg-secondary w-full md:w-60 h-15 md:h-full fixed bottom-0 md:top-15 flex items-center md:items-start md:pt-6">
+			<section className="wrapper md:ml-0 flex md:flex-col items-center md:items-start justify-between gap-2">
+				<NavLink
+					to="/note"
+					className="link w-8 md:w-[90%] h-8 md:h-9 flex justify-center md:justify-start items-center md:gap-2 md:pl-9 md:rounded-l-none md:rounded-r-xl md:border-0"
+				>
+					<TfiPencilAlt2 className="fill-inherit w-5 md:w-6 h-5 md:h-6" />
+					<span className="hidden md:block text-inherit md:text-xl">Note</span>
+				</NavLink>
+				<NavLink
+					to="/archive"
+					className="link w-8 md:w-[90%] h-8 md:h-9 flex justify-center md:justify-start items-center md:gap-2 md:pl-9 md:rounded-l-none md:rounded-r-xl md:border-0"
+				>
+					<MdOutlineArchive className="fill-inherit w-5 md:w-6 h-5 md:h-6" />
+					<span className="hidden md:block text-inherit md:text-xl">Archive</span>
+				</NavLink>
+				<NavLink
+					to="/trash"
+					className="link w-8 md:w-[90%] h-8 md:h-9 flex justify-center md:justify-start items-center md:gap-2 md:pl-9 md:rounded-l-none md:rounded-r-xl md:border-0"
+				>
+					<BsTrash className="fill-inherit w-5 md:w-6 h-5 md:h-6" />
+					<span className="hidden md:block text-inherit md:text-xl">Trash</span>
+				</NavLink>
+			</section>
+		</article>
+	);
+};
