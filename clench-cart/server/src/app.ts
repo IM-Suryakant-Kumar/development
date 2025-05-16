ef036c363dde226eda66915f2e4218729c5f3443
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-05-16 15:20:44 +0530

    feat: updated clech-cart

--- clench-cart/server/src/app.ts
+++ clench-cart/server/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use(morgan("dev"));
 
 app.use(notFoundMiddleware);
