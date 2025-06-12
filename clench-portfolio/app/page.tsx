d6d221495543cd6f193969c1009f50eda933d23e
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-06-12 23:20:59 +0530

    completed portfolio

--- clench-portfolio/app/page.tsx
+++ clench-portfolio/app/page.tsx
@@ -1,9 +1,16 @@
+import About from "@/components/About";
+import Blogs from "@/components/Blogs";
+import Contact from "@/components/Contact";
+import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+
 export default function Home() {
   return (
     <>
-    <Hero />
-    <About />
+    <Intro />
     <Skills />
+    <About />
     <Projects />
     <Blogs />
     <Contact />
