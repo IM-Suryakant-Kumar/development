d77a8a0fa8682f7f7dda078af4be4c52f9c1efd7
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-07-13 11:45:09 +0530

    feat: Updated portfolio

--- 01-portfolio/app/layout.tsx
+++ 01-portfolio/app/layout.tsx
@@ -1,34 +1,29 @@
 import type { Metadata } from "next";
 import { Josefin_Sans, Cinzel } from "next/font/google";
-import "./globals.css";
+import "./global.css";
 
 const josefin = Josefin_Sans({
   variable: "--font-josefin",
   subsets: ["latin"],
-});
+})
 
 const cinzel = Cinzel({
   variable: "--font-cinzel",
-  subsets: ["latin"],
-});
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Suryakant | Portfolio",
-  description: "Hi, I'm Suryakant. I am a Frontend Developer. this is my portfolio. where you can explore my projects, blogs and skills and my learning journey.",
-};
+  description: "Hi, I'm Suryakantv Kumar. A Frontend Developer from India. This is my portfolio where you can find my projects, blogs, skills and my learning journey."
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
-      <body
-        className={`${josefin.variable} ${cinzel.variable} antialiased`}
-      >
+      <body className={`${josefin.variable} ${cinzel.variable} antialized`}>
         {children}
       </body>
     </html>
-  );
+  )
 }
+
