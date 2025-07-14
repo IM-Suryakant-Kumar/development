9b0252c1776940f00993ec47b083810d01f6da70
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-07-14 19:45:04 +0530

    delete some projects

--- 10-blog/src/app/page.js
+++
@@ -1,7 +0,0 @@
-import { getBlogPostsAction } from "@/actions/blog";
-import HomeComponent from "@/components/home";
-
-export default async function Home() {
-  const posts = await getBlogPostsAction();
-  return <HomeComponent posts={posts.posts} />;
-}
