a6589a3fc8c4e3a589448ff37285ddaea09d9c09
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-08-28 15:12:33 +0530

    added webhooks

--- ccpp1/src/app/sign-in/[[...sign-in]]/page.tsx
+++ ccpp1/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="min-h-[calc(100vh-64px)] flex items-center justify-center">
       <SignIn />
     </div>
   );
