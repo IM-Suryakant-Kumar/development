0cabf4f96c38434627166f16a791880e7f1c3526
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-08-30 10:37:06 +0530

    Updated prisma schema

--- ccpp1/src/components/Header.tsx
+++ ccpp1/src/components/Header.tsx
@@ -11,11 +11,7 @@ export default function Header() {
     <header className="flex h-16 items-center justify-end gap-4 p-4">
       <SignedOut>
         <SignInButton />
-        <SignUpButton>
-          <button className="text-ceramic-white h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium sm:h-12 sm:px-5 sm:text-base">
-            Sign Up
-          </button>
-        </SignUpButton>
+        <SignUpButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
