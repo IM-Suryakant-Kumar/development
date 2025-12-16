b538f32a0eb8a1d95e333fa2aea781c50bd3f9bc
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-12-16 17:05:11 +0530

    feat: Search using debounce and throttle

--- 11-practice-projects/src/App.tsx
+++ 11-practice-projects/src/App.tsx
@@ -3,10 +3,20 @@ import StarRating from "./components/StarRating";
 import Search from "./components/Search";
 
 const App = () => {
+	const projects = [
+		{ heading: "1. Star Rating", component: StarRating },
+		{ heading: "1. Star Rating", component: Search },
+	] as const;
+
 	return (
 		<div className="flex flex-col gap-6 p-4">
-			<WrappedComp heading="1. Star Rating" Component={StarRating} />
-			<WrappedComp heading="2. Search bar with debouncing" Component={Search} />
+			{projects.map((project) => (
+				<WrappedComp
+					key={project.heading}
+					heading={project.heading}
+					Component={project.component}
+				/>
+			))}
 		</div>
 	);
 };
--- 11-practice-projects/src/components/Search.tsx
+++ 11-practice-projects/src/components/Search.tsx
@@ -1,9 +1,66 @@
-const Search = () => {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="bg-gray-200 w-sm h-7 rounded"></div>
-    </div>
-  )
-}
+import React, { useMemo, useState } from "react";
 
-export default Search
+const Search = () => {
+	const [search, setSearch] = useState("");
+
+	function debounce(
+		fn: (...args: React.ChangeEvent<HTMLInputElement>[]) => void
+	) {
+		let timeoutId: number;
+
+		return (...args: React.ChangeEvent<HTMLInputElement>[]) => {
+			if (timeoutId) clearTimeout(timeoutId);
+			timeoutId = setTimeout(() => {
+				fn(...args);
+			}, 1000);
+		};
+	}
+
+	function throttle(
+		fn: (...args: React.ChangeEvent<HTMLInputElement>[]) => void
+	) {
+		let inThrottle: boolean;
+
+		return (...args: React.ChangeEvent<HTMLInputElement>[]) => {
+			if (!inThrottle) {
+				fn(...args);
+				inThrottle = true;
+				setTimeout(() => {
+					inThrottle = false;
+				}, 1000);
+			}
+		};
+	}
+
+	const debounceChangehandler = useMemo(
+		() =>
+			debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+				setSearch(e.target.value);
+			}),
+		[]
+	);
+
+	const throttleChangehandler = useMemo(
+		() =>
+			throttle((e: React.ChangeEvent<HTMLInputElement>) => {
+				setSearch(e.target.value);
+			}),
+		[]
+	);
+
+	return (
+		<div className="flex flex-col justify-center items-center">
+			<div className="w-sm px-2 py-1 border-2 border-gray-600 rounded">
+				<input
+					type="search"
+					className="w-full outline-0"
+					placeholder="Search"
+					onChange={throttleChangehandler}
+				/>
+			</div>
+			<p className="w-sm">{search}</p>
+		</div>
+	);
+};
+
+export default Search;
