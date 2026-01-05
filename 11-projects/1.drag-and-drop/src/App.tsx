1970bbb10c9b8ad02e4d298e0d850cb53234741d
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-01-05 14:47:48 +0530

    feat: drag and drop

--- 11-projects/1.drag-and-drop/src/App.tsx
+++ 11-projects/1.drag-and-drop/src/App.tsx
@@ -1,18 +1,51 @@
-import { useState } from "react"
-import Notes from "./components/Notes"
+import { useState } from "react";
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    { id: 1, text: 'First Note' },
-    { id: 2, text: 'Second Note' },
-    { id: 3, text: 'Third Note' },
-  ])
+	const [columns, setColumns] = useState<{ [key: string]: string[] }>({
+		column1: ["item1", "item2"],
+		column2: ["item3", "item4"],
+	});
 
-  return (
-    <div>
-      <Notes notes={notes} setNotes={setNotes} />
-    </div>
-  )
-}
+  const onDrop = (event: React.DragEvent<HTMLDivElement>, toColumn: string) => {
+    const item = event.dataTransfer.getData("item");
+    const fromColumn = event.dataTransfer.getData("fromColumn");
 
-export default App
+    if(fromColumn === toColumn) return;
+
+    setColumns((prevColumns) => {
+      const newColumns = { ...prevColumns };
+      newColumns[fromColumn] = newColumns[fromColumn].filter(i => i !== item);
+      newColumns[toColumn] = [...newColumns[toColumn], item];
+      return newColumns;
+    });
+  }
+
+	const onDragStart = (
+		e: React.DragEvent<HTMLDivElement>,
+		item: string,
+		fromColumn: string
+	) => {
+		e.dataTransfer.setData("item", item);
+		e.dataTransfer.setData("fromColumn", fromColumn);
+	};
+
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  }
+
+	return (
+		<div className="container">
+			{Object.keys(columns).map((column) => (
+				<div className="column" key={column} onDrop={(e) => onDrop(e, column)} onDragOver={onDragOver}>
+					{columns[column].map((item) => (
+						<div draggable className="item" key={item} onDragStart={(e) => onDragStart(e, item, column)}>
+							{item}
+						</div>
+					))}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;
