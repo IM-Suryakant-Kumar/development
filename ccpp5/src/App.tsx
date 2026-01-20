a300a62bd78bfd191b871c2e13a591784455c6b2
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-01-20 17:30:28 +0530

    feat: Router setup

--- ccpp5/src/App.tsx
+++ ccpp5/src/App.tsx
@@ -1,7 +1,23 @@
-const App = () => {
-  return (
-    <div>Dark light mode</div>
-  )
-}
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Layout from "./components/Layout";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
 
-export default App
+const App = () => {
+	const router = createBrowserRouter([
+		{
+			Component: Layout,
+			children: [
+				{ path: "/", Component: Home },
+				{ path: "/about", Component: About },
+				{ path: "/blog", Component: Blog },
+			],
+		},
+		{ path: "*", Component: NotFound },
+	]);
+	return <RouterProvider router={router} />;
+};
+
+export default App;
