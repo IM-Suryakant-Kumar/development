5bf7b8684ab151df4508e7a89a87781611dc5104
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-02-04 22:26:06 +0530

    feat: complet NotFOund page

--- ccpp8/client/src/pages/NotFound.tsx
+++ ccpp8/client/src/pages/NotFound.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router";
+
 export const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+	return (
+		<div className="min-h-screen flex flex-col justify-center items-center gap-4 text-center">
+			<h1 className="text-gray-600 text-2xl font-semibold">
+				404 - PAGE NOT FOUND
+			</h1>
+			<Link
+				to="/"
+				className="bg-gray-400 text-white border border-gray-300 rounded px-8 py-1"
+			>
+				Home
+			</Link>
+		</div>
+	);
+};
