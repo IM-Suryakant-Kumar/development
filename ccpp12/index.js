8dd90284ffb7af6b08beb6290e587426bc9aa2ab
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-05-24 16:24:04 +0530

    backend setup

---
+++ ccpp12/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {createServer} from "http";
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+import {Server} from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
