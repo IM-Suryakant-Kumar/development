17fa7c2ac937c0aba0103044ce93099d27e33333
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-07-04 16:42:22 +0530

    feat: Added socket connection

--- ccpp13/server/src/app.ts
+++ ccpp13/server/src/app.ts
@@ -1,20 +1,39 @@
 import "dotenv/config";
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 
-const app = express();
 const PORT = process.env.PORT;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+	cors: { origin: "*" },
+});
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 
+const ROOM = "group";
+
+io.on("connection", (socket) => {
+	console.log("A user connected", socket.id);
+
+	socket.on("joinRoom", async (userName) => {
+		console.log(`${userName} is joining the room`);
+
+		await socket.join(ROOM);
+	});
+});
+
 (async () => {
 	try {
-		app.listen(PORT, () => {
+		server.listen(PORT, () => {
 			console.log(`Server is running on  http://localhost:${PORT}`);
 		});
 	} catch (error) {
